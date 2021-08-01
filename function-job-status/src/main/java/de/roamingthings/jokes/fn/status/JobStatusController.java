1c16999d22976d4a35890bbb769582500fcc75a3
Alexander Sparkowsky <info@roamingthings.de>
2021-08-01 11:52:32 +0200

    Provide DynamoDB client in job status function as singleton

--- function-job-status/src/main/java/de/roamingthings/jokes/fn/status/JobStatusController.java
+++ function-job-status/src/main/java/de/roamingthings/jokes/fn/status/JobStatusController.java
@@ -6,14 +6,8 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
-import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.inject.Inject;
 
 @Controller
 @Introspected
@@ -21,53 +15,14 @@ public class JobStatusController {
 
     private static final Logger log = LoggerFactory.getLogger(JobStatusController.class);
 
-    private static final String STATUS_COLUMN = "Status";
-    private static final String TEXT_COLUMN = "Text";
+    @Inject
+    private JobStatusRepository jobStatusRepository;
 
     @Get("/jobs/{jobId}")
     public JobStatus retrieveJobStatus(@QueryValue("jobId") String jobId) {
         log.info("Getting status for job <{}>", jobId);
 
-        return getItem(jobId);
-    }
-
-    private DynamoDbClient getClient() {
-        return DynamoDbClient.builder()
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .build();
-    }
-
-    // Get a single item from the Work table based on the Key.
-    public JobStatus getItem(String idValue) {
-        var jobTable = System.getenv("JOB_TABLE");
-
-        var ddbClient = getClient();
-
-        var keyToGet = new HashMap<String, AttributeValue>();
-        keyToGet.put("Id", AttributeValue.builder()
-                .s(idValue)
-                .build());
-
-        var request = GetItemRequest.builder()
-                .key(keyToGet)
-                .tableName(jobTable)
-                .build();
-
-        GetItemResponse retrievedItem = ddbClient.getItem(request);
-        if (!retrievedItem.hasItem()) {
-            return null;
-        }
-
-        Map<String, AttributeValue> returnedItem = retrievedItem.item();
-        var jobStatus = new JobStatus();
-        jobStatus.setId(idValue);
-        if (returnedItem.containsKey(STATUS_COLUMN)) {
-            jobStatus.setStatus(returnedItem.get(STATUS_COLUMN).s());
-        }
-        if (returnedItem.containsKey(TEXT_COLUMN)) {
-            jobStatus.setText(returnedItem.get(TEXT_COLUMN).s());
-        }
-        return jobStatus;
+        return jobStatusRepository.fetchJobStatusById(jobId);
     }
 }
 
