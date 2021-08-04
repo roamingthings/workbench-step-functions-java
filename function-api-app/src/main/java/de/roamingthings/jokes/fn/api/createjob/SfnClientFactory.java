e9cbb82cf8e418326836f236ea7a0537cd516dac
Alexander Sparkowsky <info@romaingthings.de>
2021-08-04 14:40:09 +0200

    Add explicit region configuration

--- function-api-app/src/main/java/de/roamingthings/jokes/fn/api/createjob/SfnClientFactory.java
+++ function-api-app/src/main/java/de/roamingthings/jokes/fn/api/createjob/SfnClientFactory.java
@@ -11,6 +11,8 @@ import software.amazon.awssdk.services.sfn.SfnClient;
 
 import javax.inject.Singleton;
 
+import static software.amazon.awssdk.regions.Region.EU_CENTRAL_1;
+
 @Factory
 @Introspected
 public class SfnClientFactory {
@@ -23,7 +25,9 @@ public class SfnClientFactory {
     SfnClient sfnClientProd() {
         log.info("Setting AWS Credentials from Attached Role attached to Lambda Function.");
         log.info("See this - https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html");
-        return SfnClient.builder().build();
+        return SfnClient.builder()
+                .region(EU_CENTRAL_1)
+                .build();
     }
 
     @Bean
@@ -33,6 +37,7 @@ public class SfnClientFactory {
         log.info("Setting EnvironmentVariableCredentialsProvider for AWS Credentials which would fetch credentials from Environment variables.");
         log.info("See this to set AWS Credentials Locally - https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html");
         return SfnClient.builder()
+                .region(EU_CENTRAL_1)
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
--- function-app-job-status/src/main/java/de/roamingthings/jokes/fn/status/DynamoDbClientFactory.java
+++ function-app-job-status/src/main/java/de/roamingthings/jokes/fn/status/DynamoDbClientFactory.java
@@ -11,6 +11,8 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import javax.inject.Singleton;
 
+import static software.amazon.awssdk.regions.Region.EU_CENTRAL_1;
+
 @Factory
 @Introspected
 public class DynamoDbClientFactory {
@@ -23,7 +25,9 @@ public class DynamoDbClientFactory {
     DynamoDbClient dynamoDbClientProd() {
         log.info("Setting AWS Credentials from Attached Role attached to Lambda Function.");
         log.info("See this - https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html");
-        return DynamoDbClient.builder().build();
+        return DynamoDbClient.builder()
+                .region(EU_CENTRAL_1)
+                .build();
     }
 
     @Bean
@@ -33,6 +37,7 @@ public class DynamoDbClientFactory {
         log.info("Setting EnvironmentVariableCredentialsProvider for AWS Credentials which would fetch credentials from Environment variables.");
         log.info("See this to set AWS Credentials Locally - https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html");
         return DynamoDbClient.builder()
+                .region(EU_CENTRAL_1)
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
